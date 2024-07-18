c9f4d67a0802191c58eeb05a76b38eb9c79abbc1
thanhITSW <nguyenmtthanh13@gmail.com>
2024-07-18 16:16:07 +0700

    fix bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const cors = require('cors')
-const session = require('cookie-session');
+const session = require('express-session')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const flash = require('express-flash')
